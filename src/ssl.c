2a2c7db5d1acd87ae2fab936402e70b7ae3d0226
overflowerror <mail@overflowerror.com>
2019-03-13 18:34:13 +0100

    I should compile this locally

--- src/ssl.c
+++ src/ssl.c
@@ -52,7 +52,7 @@ void* copyFromSslToFd(void* data) {
 	}
 
 	debug("ssl: read thread finished");
-	error("ssl: failed to create new connection: %s", ERR_error_string(b), NULL));
+	error("ssl: failed to create new connection: %s", ERR_error_string(b, NULL));
 
 	return NULL;
 }
